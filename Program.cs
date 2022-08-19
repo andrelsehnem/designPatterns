d56d97a6dd1d556b1504226948ef576ca5df55d7
André Luis Sehnem <als10andr@hotmail.com>
2022-08-19 17:51:25 -0300

    Menu funcional, criação das pastas

--- Program.cs
+++ Program.cs
@@ -8,32 +8,43 @@ namespace designPatterns
     {
         public static void Main()
         {
-            
             new Program();
         }
 
         public Program(){
-            Console.WriteLine("1- Criacionais");
-            Console.WriteLine("2- Estruturais");
-            Console.WriteLine("3- Comportamentais");
+            Console.Clear();
             bool a = true;
             while(a){
+                callMenu();
                 switch (Console.ReadLine()){
                 case "1":
+                    Console.Clear();
                     Console.WriteLine("a");
                     break;
                 case "2":
+                    Console.Clear();
                     Console.WriteLine("b");
                     break;
                 case "3":
+                    Console.Clear();
                     Console.WriteLine("c");
                     break;
+                case "x":
+                    a = false;
+                    break;
                 default:
-                    new Program();
+                    Console.Clear();
                     break;
                 }
             }
             Console.WriteLine("fim");
         } 
+
+
+        public void callMenu(){
+            Console.WriteLine("1- Criacionais");
+            Console.WriteLine("2- Estruturais");
+            Console.WriteLine("3- Comportamentais");
+        }
     }
 }
