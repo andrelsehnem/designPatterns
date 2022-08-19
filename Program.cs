a620862c1768293cc382904049fa9495039f3f6f
André Luis Sehnem <als10andr@hotmail.com>
2022-08-19 16:02:28 -0300

    Inicio

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace designPatterns 
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+            new Program();
+        }
+
+        public Program(){
+            Console.WriteLine("1- Criacionais");
+            Console.WriteLine("2- Estruturais");
+            Console.WriteLine("3- Comportamentais");
+            bool a = true;
+            while(a){
+                switch (Console.ReadLine()){
+                case "1":
+                    Console.WriteLine("a");
+                    break;
+                case "2":
+                    Console.WriteLine("b");
+                    break;
+                case "3":
+                    Console.WriteLine("c");
+                    break;
+                default:
+                    new Program();
+                    break;
+                }
+            }
+            Console.WriteLine("fim");
+        } 
+    }
+}
